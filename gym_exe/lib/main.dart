c5455e9ea44dc516d29e96f85dd2cb665a88ebd7
Hellen Cristina Gurgacz <63474327+hellengurgaczz@users.noreply.github.com>
2022-09-12 19:58:42 -0300

    Update main.dart

--- gym_exe/lib/main.dart
+++ gym_exe/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.black,
       ),
-      home: const MyHomePage(title: 'Gym.exe'),
+      home: const MyHomePage(title: 'Ticket4U'),
     );
   }
 }
