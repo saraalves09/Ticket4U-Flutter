ca56a09657a08250dac030645020be62dcf5bebd
Hellen Gurgacz <hellen.fiori@clinicarx.com.br>
2022-09-12 09:31:52 -0300

    projeto basico flutter

---
+++ gym_exe/android/app/src/main/kotlin/br/com/hellen/gym_exe/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.hellen.gym_exe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gym_exe/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gym.exe',
+      theme: ThemeData(
+        primaryColor: Colors.black,
+      ),
+      home: const MyHomePage(title: 'Gym.exe'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(widget.title),
+        ),
+      ),
+      body: Center(),
+    );
+  }
+}
